97af60790b8954317b6f5b85cbca7a8357a10fa2
OZiyuTao <ziyu.tao@orthogonal.cc>
2024-07-02 13:55:13 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Become Agile</h1>
+      <div className="card">Your Agile Mate</div>
+    </>
+  );
+}
+
+export default App;
